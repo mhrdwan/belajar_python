1bbe6468ce9926bbf3ce5977592806d535927e8d
mhrdwan <futuremhr@gmail.com>
2023-09-22 23:09:18 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+def namalo(satu, dua):
+    nama_pengguna = input("Siapa Nama Anda? ")
+    print(f"nama lo {nama_pengguna}")
+    if nama_pengguna == "huyu":
+        print("bukan, namalo itu adalah kocak")
+        total = satu + dua
+        print(f"total {satu} + {dua} = {total}")
+
+namalo(5, 10)
---
+++ test.js
@@ -0,0 +1,3 @@
+for (let index = 100; index % 2 === 0; index++) {
+    console.log(`hasil ganjil`);
+}
